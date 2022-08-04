d1db4974a59eedb31a2281cdc94743f83c884898
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2022-08-03 23:13:40 -0500

    Complete 'Hashing' assignment

--- week4/hashing/hashing.py
+++ week4/hashing/hashing.py
@@ -6,15 +6,34 @@ class HashTable:
 
     def hashfunction(self,key):
         # Insert your hashing function code
+        keystr = str(key)
+        hashVal = 0
+        for ch in str(keystr):
+            hashVal += ord(ch)
+        return (hashVal % self.size)
 
     def rehash(self,key):
         # Insert your secondary hashing function code
+        keystr = str(key)
+        hashVal = 0
+        count = 0
+        for ch in str(keystr):
+            count += 1
+            hashVal += ord(ch) + count
+        return (hashVal % self.size)
 
     def put(self,key,data):
         # Insert your code here to store key and data 
+        newPos = self.hashfunction(key)
+        while self.slots[newPos] and self.data[newPos]:
+            newPos = self.rehash(key)
+            break
+        self.data[newPos] = data
+        self.slots[newPos] = key
 
     def get(self,key):
         # Insert your code here to get data by key
+        return self.data[self.hashfunction(key)]
 
     def __getitem__ (self,key):
         return self.get(key)
@@ -25,11 +44,22 @@ class HashTable:
 
 H = HashTable()
 H[69] = 'A'
+
 # Store remaining input data
+H.put(66, 'B')
+H.put(80, 'C')
+H.put(35, 'D')
+H.put(18, 'E')
+H.put(52, 'F')
+H.put(89, 'G')
+H.put(70, 'H')
+H.put(12, 'I')
 
 # print the slot values
+print(H.slots)
 
 # print the data values
+print(H.data)
 
 # print the value for key 52
-
+print(H.get(52))
